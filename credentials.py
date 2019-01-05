a7aeb84ed68a48e40707855c135ca63601070a01
Luc Bertin <lucbertin@outlook.com>
2019-01-05 20:51:46 +0100

    reset data

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # Here you can put login details
 # I strongly recommend you to use fake account
 
-USER_ID = 'xxxxxxxxxxxxx@xxxx.xx'
+USER_ID = 'xxxxxxxxxxxx@xxxxx.xx'
 USER_PASSWORD = 'xxxxxxxxxxxxxxx'
