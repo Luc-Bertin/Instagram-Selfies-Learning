08e4fa42e12aafc84f0e538b4e0e227c5d5610f4
Luc Bertin <lucbertin@outlook.com>
2019-01-05 20:27:25 +0100

    credentials to login Instagram

--- credentials.py
+++ credentials.py
@@ -1,2 +1,5 @@
-USER_ID = 'alexandroboteglia@gmail.com'
-USER_PASSWORD = 'A12F367GTo0552'
+# Here you can put login details
+# I strongly recommend you to use fake account
+
+USER_ID = 'xxxxxxxxxxxxx@xxxx.xx'
+USER_PASSWORD = 'xxxxxxxxxxxxxxx'
