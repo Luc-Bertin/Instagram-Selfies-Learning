a82836ca8163dbfef39f2b5180a1195d34960487
Luc Bertin <46030499+Luc-Bertin@users.noreply.github.com>
2019-02-01 20:25:24 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,10 +1,5 @@
 # Here you can put login details
 # I strongly recommend you to use fake account
 
-<<<<<<< HEAD
-USER_ID = 'alexandroboteglia@gmail.com'
-USER_PASSWORD = 'A12F367GTo0552'
-=======
 USER_ID = 'xxxxxxxxxxxx@xxxxx.xx'
 USER_PASSWORD = 'xxxxxxxxxxxxxxx'
->>>>>>> a7aeb84ed68a48e40707855c135ca63601070a01
